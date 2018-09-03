a4b0bb424856fe8583c171ae9f931b99dd6032a9
Diego Calero <dcalero@fiqus.com>
2018-09-03 02:36:31 -0300

    Completed files for class 1.

--- clase1/01.hello-world.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello World! I'm console.log()");
-console.debug("Hello World! I'm console.debug()");
-console.error("Hello World! I'm console.error()");
-console.trace("Hello World! I'm console.trace()");

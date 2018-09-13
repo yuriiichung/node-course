913355fb670c0132da1474fe5ad19b9634730229
Diego Calero <dcalero@fiqus.com>
2018-09-13 02:02:08 -0300

    Adding file class3/11.sinon.tests.js and updating package.json.

---
+++ class3/11.sinon.tests.js
@@ -0,0 +1,99 @@
+const sinon = require("sinon"),
+  expect = require("chai").expect,
+  testObject = {
+    salute: (name) => {
+      return `Hola ${name}!`;
+    },
+    execute: (fn, param) => {
+      return fn(param);
+    }
+  };
+
+describe("sinon.spy()", () => {
+
+  it("testObject.execute() should call the given function", () => {
+    const spy = sinon.spy();
+
+    testObject.execute(spy, "Diego");
+
+    expect(spy.calledOnce).to.eql(true);
+    expect(spy.getCall(0).args[0]).to.eql("Diego");
+  });
+
+  it("testObject.salute() should be called twice", () => {
+    const spy = sinon.spy(testObject, "salute");
+
+    testObject.salute("Diego");
+    testObject.execute(testObject.salute, "Manuel");
+
+    testObject.salute.restore();
+    expect(spy.calledTwice).to.eql(true);
+    expect(spy.withArgs("Diego").calledOnce).to.eql(true);
+    expect(spy.withArgs("Manuel").calledOnce).to.eql(true);
+    expect(spy.getCall(0).args[0]).to.eql("Diego");
+    expect(spy.getCall(1).args[0]).to.eql("Manuel");
+  });
+
+});
+
+describe("sinon.stub()", () => {
+
+  it("testObject.execute() should return the value given at stub().returns()", () => {
+    const stub = sinon.stub().returns(22),
+      result = testObject.execute(stub, "Diego");
+
+    expect(result).to.eql(22);
+    expect(stub.calledOnce).to.eql(true);
+    expect(stub.getCall(0).args[0]).to.eql("Diego");
+  });
+
+  it("testObject.execute() should return different values on consecutive calls", () => {
+    const stub = sinon.stub();
+
+    stub.onCall(0).returns(11);
+    stub.onCall(1).returns(22);
+    stub.returns(33);
+
+    expect(testObject.execute(stub, "something")).to.eql(11);
+    expect(testObject.execute(stub, "whatever")).to.eql(22);
+    expect(testObject.execute(stub, "anything")).to.eql(33);
+    expect(testObject.execute(stub, "nothing")).to.eql(33);
+    expect(stub.callCount).to.eql(4);
+  });
+
+  it("testObject.salute() should call the function defined at stub().callsFake()", () => {
+    const stub = sinon.stub(testObject, "salute").callsFake((name) => {
+        return `Chau ${name}!`;
+      }),
+      result1 = testObject.salute("Diego"),
+      result2 = testObject.execute(testObject.salute, "Manuel");
+
+    testObject.salute.restore();
+    expect(result1).to.eql("Chau Diego!");
+    expect(result2).to.eql("Chau Manuel!");
+    expect(stub.calledTwice).to.eql(true);
+    expect(stub.withArgs("Diego").calledOnce).to.eql(true);
+    expect(stub.withArgs("Manuel").calledOnce).to.eql(true);
+    expect(stub.getCall(0).args[0]).to.eql("Diego");
+    expect(stub.getCall(1).args[0]).to.eql("Manuel");
+  });
+
+});
+
+describe("sinon.mock()", () => {
+
+  it("testObject.salute() should be called twice", () => {
+    const mock = sinon.mock(testObject);
+
+    mock.expects("salute").twice().returns(22);
+    mock.expects("execute").once().callsFake((fn) => {
+      fn();
+    });
+
+    testObject.salute("Diego");
+    testObject.execute(testObject.salute, "Manuel");
+
+    mock.verify();
+  });
+
+});

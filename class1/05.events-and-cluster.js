b0902e621f109edfacecb2af3b52683811b2aea1
Diego Calero <dcalero@fiqus.com>
2018-09-15 17:18:48 -0300

    Adding more comments and improving class1/05.events-and-cluster.js.

--- class1/05.events-and-cluster.js
+++ class1/05.events-and-cluster.js
@@ -3,13 +3,15 @@
 const cluster = require("cluster"),
   http = require("http");
 
+// This works for both main process and workers
 process.on("uncaughtException", (err) => {
-  console.error("UNCAUGHT EXCEPTION", err);
+  console.error(`UNCAUGHT EXCEPTION AT PID #${process.pid}`, err);
   process.exit(1);
 });
 
+// This works for both main process and workers
 process.on("unhandledRejection", (err) => {
-  console.error("UNHANDLED REJECTION", err);
+  console.error(`UNHANDLED REJECTION AT PID #${process.pid}`, err);
   process.exit(2);
 });
 
@@ -33,6 +35,9 @@ if (cluster.isMaster) {
       } else if (numReqs === 6) {
         // Test the uncaughtRejection message
         worker.send({cmd: "throwUncaughtRejection"});
+      } else if (numReqs === 10) {
+        // Test the uncaughtRejection (on master, should kill it and all its workers)
+        throw new Error(`This exception is not caught at main #${process.pid}!`);
       } else {
         // Tell the worker to send the response with numReqs
         worker.send({cmd: "notifyResponse", numReqs});
@@ -47,8 +52,16 @@ if (cluster.isMaster) {
   }
 
   for (const id in cluster.workers) {
+    // Listen on "message" event for each worker but at main process side
     cluster.workers[id].on("message", messageHandler);
   }
+
+  // This is another (easiest) way to listen for workers messages
+  cluster.on("message", (worker, message) => {
+    console.log(`Worker #${worker.process.pid} sent a message to master!`);
+    console.log("Message: ", message);
+    // messageHandler(message); We could call messageHandler() here too
+  });
 
   cluster.on("exit", (worker, code, signal) => {
     console.log(`Worker #${worker.process.pid} died with code ${code} and signal ${signal}`);
@@ -64,8 +77,8 @@ if (cluster.isMaster) {
       // Notify master about the request
       process.send({id: cluster.worker.id, cmd: "notifyRequest"});
 
-      // Wait the master answer to send the response
-      process.on("message", (msg) => {
+      // Wait the master answer to send the response (NOTICE: we use once instead of on)
+      process.once("message", (msg) => {
         if (msg.cmd === "notifyResponse") {
           res.writeHead(200);
           res.end(`Hello world! This is worker #${process.pid} at request#${msg.numReqs}\n`);

5c43ae827c77b8ce2542a22b7529692a63936bf9
Diego Calero <dcalero@fiqus.com>
2018-09-16 21:26:18 -0300

    Finishing web excercise!

--- exercises/web/app.js
+++ exercises/web/app.js
@@ -1,5 +1,16 @@
 const express = require("express"),
-  app = express();
+  app = express(),
+  bodyParser = require("body-parser"),
+  morgan = require("morgan"),
+  helmet = require("helmet"),
+  cluster = require("cluster"),
+  numCPUs = require("os").cpus().length;
+
+app.set("view engine", "ejs");
+app.set("views", `${__dirname}/views`); // Set the views folder!
+// Give our app support to parse JSON data on form POST requests and make it available at req.body
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- solved/web/app.js
+++ solved/web/app.js
@@ -1,17 +1,39 @@
 const express = require("express"),
   app = express(),
-  cluster = require("cluster"),
+  bodyParser = require("body-parser"),
   morgan = require("morgan"),
   helmet = require("helmet"),
+  cluster = require("cluster"),
   numCPUs = require("os").cpus().length;
 
 app.set("view engine", "ejs");
-app.set("views", `${__dirname}/views`);
+app.set("views", `${__dirname}/views`); // Set the views folder!
+// Give our app support to parse JSON data on form POST requests and make it available at req.body
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(helmet());
 
+app.use((req, res, next) => {
+  process.send("request");
+  next();
+});
+
+function displayForm(res, {name = "", age = "", text = ""}) {
+  res.render("form", {name, age, text});
+}
+
 app.get("/", (req, res) => {
   res.render("index", {title: "Hey", message: "Hello there!"});
+});
+
+app.get("/form", (req, res) => {
+  displayForm(res, req.query);
+});
+
+app.post("/form/send", (req, res) => {
+  console.log("Received form data:", req.body);
+  displayForm(res, req.body);
 });
 
 if (cluster.isMaster) {
@@ -20,6 +42,11 @@ if (cluster.isMaster) {
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
+
+  let reqNum = 0;
+  cluster.on("message", (worker) => {
+    console.log(`Request number #${++reqNum} at PID #${worker.process.pid}`);
+  });
 
   cluster.on("exit", (worker, code, signal) => {
     console.log(`Worker #${worker.process.pid} died with code ${code} and signal ${signal}`);
